e34351d89e6ad096826fb3cb9d38647eaf2c4d9d
mauricioww <marcos_c79@hotmail.com>
2020-12-02 22:50:20 -0600

    scrum

--- scrum/scrum/urls_2.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import url, include
-from rest_framework.authtoken.views import obtain_auth_token
-from board.urls import router
-
-urlpatterns = [
-    url(r'^api/token/', obtain_auth_token, name='api-token'),
-    url(r'^api/', include(router.urls)),
-]
